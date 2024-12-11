a7633b194a54ca2c3233f0599e4203705137a50a
Hans Rudel <jakub.lubsz@gmail.con>
2024-12-11 20:32:28 +0100

    Aktualizacja

--- Egzamin_2/skrypt.js
+++ Egzamin_2/skrypt.js
@@ -1,30 +1,28 @@
-// Funkcja do obsługi kliknięć na cytaty
-function toggleQuotes(clickedId) {
-    // Pobieramy wszystkie elementy cytatów
-    const quote1 = document.getElementById('osoba1');
-    const quote2 = document.getElementById('osoba2');
-    const quote3 = document.getElementById('osoba3');
+function zmianaCytatu(clickedId) {
     
-    // Na podstawie kliknięcia zmieniamy widoczność cytatów
+    const cytat1 = document.getElementById('osoba1');
+    const cytat2 = document.getElementById('osoba2');
+    const cytat3 = document.getElementById('osoba3');
+    
+    
     if (clickedId === 'osoba1') {
-        quote1.style.display = 'none';  // Ukryj pierwszy cytat
-        quote2.style.display = 'block'; // Pokaż drugi cytat
+        cytat1.style.display = 'none';  
+        cytat2.style.display = 'block'; 
     } else if (clickedId === 'osoba2') {
-        quote2.style.display = 'none';  // Ukryj drugi cytat
-        quote3.style.display = 'block'; // Pokaż trzeci cytat
+        cytat2.style.display = 'none'; 
+        cytat3.style.display = 'block'; 
     } else if (clickedId === 'osoba3') {
-        quote3.style.display = 'none';  // Ukryj trzeci cytat
-        quote1.style.display = 'block'; // Pokaż pierwszy cytat
+        cytat3.style.display = 'none';  
+        cytat1.style.display = 'block'; 
     }
 }
 
-// Dodajemy nasłuch na kliknięcia dla każdego cytatu
 document.getElementById('osoba1').addEventListener('click', function() {
-    toggleQuotes('osoba1');
+    zmianaCytatu('osoba1');
 });
 document.getElementById('osoba2').addEventListener('click', function() {
-    toggleQuotes('osoba2');
+    zmianaCytatu('osoba2');
 });
 document.getElementById('osoba3').addEventListener('click', function() {
-    toggleQuotes('osoba3');
+    zmianaCytatu('osoba3');
 });
